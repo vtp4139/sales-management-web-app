06e15512c40742dedf8d285879fb867d186bbdc3
tuanphuong <tuanphuong4139@gmail.com>
2023-04-25 12:00:15 +0700

    Build user info UI

---
+++ src/SalesManagementWebsite.Client/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesManagementWebsite.Client.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserInfo()
+        {
+            return View();
+        }
+    }
+}
