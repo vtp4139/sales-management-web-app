00114e16bc3c658f3799db12477edb2b66c1852a
tuanphuong <tuanphuong4139@gmail.com>
2023-08-10 11:22:19 +0700

    Set up hangfire

---
+++ src/SalesManagementWebsite.Hangfire/Program.cs
@@ -0,0 +1,43 @@
+using Hangfire;
+using Hangfire.SqlServer;
+
+var builder = WebApplication.CreateBuilder(args);
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Config Hangfire
+GlobalConfiguration.Configuration.UseSqlServerStorage(defaultConnection, new SqlServerStorageOptions
+{            
+    PrepareSchemaIfNecessary = true //Fix bug: Invalid object name 'HangFire.AggregatedCounter'.
+});
+
+builder.Services.AddHangfire(x =>
+{
+    x.UseSqlServerStorage(defaultConnection);
+});
+
+builder.Services.AddHangfireServer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseHangfireDashboard();
+
+app.Run();
