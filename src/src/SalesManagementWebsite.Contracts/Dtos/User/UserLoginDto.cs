79066ad14fc9c5b7ca67b8de9a29056e9860a461
vtp4139 <tuanphuong4139@gmail.com>
2025-02-11 15:02:57 +0700

    Add validation username and password is empty when login

--- src/src/SalesManagementWebsite.Contracts/Dtos/User/UserLoginDto.cs
+++ src/src/SalesManagementWebsite.Contracts/Dtos/User/UserLoginDto.cs
@@ -4,10 +4,10 @@ namespace SalesManagementWebsite.Contracts.Dtos.User
 {
     public class UserLoginDto
     {
-        [Required(ErrorMessage = "{0} is empty!")]
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống")]
         public string UserName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "{0} is empty!")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
         public string Password { get; set; } = string.Empty;
     }
 }
