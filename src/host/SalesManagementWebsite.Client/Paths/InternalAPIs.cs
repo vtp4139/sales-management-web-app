bb22356547d035c067695c4885f6ef970d2a904b
vtp4139 <tuanphuong4139@gmail.com>
2025-04-02 14:25:03 +0700

    [Add] Controller, View category

--- src/host/SalesManagementWebsite.Client/Paths/InternalAPIs.cs
+++ src/host/SalesManagementWebsite.Client/Paths/InternalAPIs.cs
@@ -2,7 +2,14 @@
 {
     public static class InternalAPIs
     {
+        // Users
         public const string Login = "/api/users/login";
         public const string GetUser = "/api/users/{0}";
+
+        // Categories
+        public const string GetAllCategories = "/api/categories";
+        public const string CreateCategory = "/api/categories";
+        public const string UpdateCategory = "/api/categories/{0}";
+        public const string DeleteCategory = "/api/categories/{0}";
     }
 }
---
+++ src/host/SalesManagementWebsite.Client/Services/API/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using SalesManagementWebsite.Client.Paths;
+using SalesManagementWebsite.Client.Services.Intefaces;
+using SalesManagementWebsite.Contracts.Dtos.Category;
+using SalesManagementWebsite.Contracts.Dtos.Response;
+
+namespace SalesManagementWebsite.Client.Services.API
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IApiService _apiService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CategoryService(IApiService apiService, IHttpContextAccessor httpContextAccessor)
+        {
+            _apiService = apiService;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<ResponseHandle<CategoryOutputDto>> GetAllCategories()
+        {
+            var res = await _apiService.SendRequestAsync<ResponseHandle<CategoryOutputDto>>(
+                            method: HttpMethod.Get, 
+                            endpoint: InternalAPIs.GetAllCategories);
+
+            return res;
+        }
+
+        public async Task<ResponseHandle<CategoryOutputDto>> CreateCategory(CategoryCreateDto input)
+        {
+            var res = await _apiService.SendRequestAsync<ResponseHandle<CategoryOutputDto>>(
+                            method: HttpMethod.Post,
+                            endpoint: InternalAPIs.CreateCategory,
+                            requestData: input);
+
+            return res;
+        }
+
+        public async Task<ResponseHandle<CategoryOutputDto>> UpdateCategory(Guid id, CategoryInputDto input)
+        {
+            var res = await _apiService.SendRequestAsync<ResponseHandle<CategoryOutputDto>>(
+                            method: HttpMethod.Put,
+                            endpoint: string.Format(InternalAPIs.UpdateCategory, id),
+                            requestData: input);
+
+            return res;
+        }
+
+        public async Task<ResponseHandle<CategoryOutputDto>> DeleteCategory(Guid id)
+        {
+            var res = await _apiService.SendRequestAsync<ResponseHandle<CategoryOutputDto>>(
+                            method: HttpMethod.Delete,
+                            endpoint: string.Format(InternalAPIs.DeleteCategory, id));
+
+            return res;
+        }
+    }
+}
